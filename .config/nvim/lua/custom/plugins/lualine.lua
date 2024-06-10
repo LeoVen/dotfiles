3646b7674a574e88482d75f9b18cd9b39487da57
Leonardo Vencovsky <lvenk26@gmail.com>
2024-06-10 18:31:35 +0200

    Update neovim config

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -2,6 +2,13 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
     config = function()
-        require('lualine').setup { options = { theme = 'catppuccin' } }
+        require('lualine').setup {
+            options = {
+                theme = 'catppuccin',
+            },
+            sections = {
+                lualine_c = { { 'filename', path = 1 } },
+            },
+        }
     end,
 }

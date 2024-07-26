1d4cc7ee3cab351e0080929915743c3068ff249c
Leonardo Vencovsky <lvenk26@gmail.com>
2024-07-26 22:01:58 +0200

    Fix neovim config

--- .config/nvim/lua/custom/plugins/whichkey.lua
+++ .config/nvim/lua/custom/plugins/whichkey.lua
@@ -5,20 +5,23 @@ return {
         config = function() -- This is the function that runs, AFTER loading
             require('which-key').setup()
 
-            -- Document existing key chains
-            require('which-key').register {
-                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-                ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-                ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+            require('which-key').add {
+                {
+                    { '<leader>c', '[C]ode' },
+                    { '<leader>d', '[D]ocument' },
+                    { '<leader>r', '[R]ename' },
+                    { '<leader>s', '[S]earch' },
+                    { '<leader>w', '[W]orkspace' },
+                    { '<leader>t', '[T]oggle' },
+                    { '<leader>h', 'Git [H]unk' },
+                },
+                {
+                    mode = 'v',
+                    { '<leader>h', 'Git [H]unk' },
+                },
             }
-            -- visual mode
-            require('which-key').register({
-                ['<leader>h'] = { 'Git [H]unk' },
-            }, { mode = 'v' })
+
+            require('which-key').add {}
         end,
     },
 }

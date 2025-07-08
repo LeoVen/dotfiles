5d289ec8be5c0a75b0e432a6fa7ce76692ae6363
Leonardo Vencovsky <lvenk26@gmail.com>
2025-07-08 10:35:07 +0200

    Update neovim config

--- nvim/.config/nvim/lua/custom/lsp/init.lua
+++ nvim/.config/nvim/lua/custom/lsp/init.lua
@@ -155,20 +155,7 @@ return {
                 jsonls = require 'custom.lsp.jsonls',
                 yamlls = require 'custom.lsp.yamlls',
 
-                lua_ls = {
-                    -- cmd = {...},
-                    -- filetypes = { ...},
-                    -- capabilities = {},
-                    settings = {
-                        Lua = {
-                            completion = {
-                                callSnippet = 'Replace',
-                            },
-                            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                            -- diagnostics = { disable = { 'missing-fields' } },
-                        },
-                    },
-                },
+                lua_ls = {},
             }
 
             -- Ensure the servers and tools above are installed
@@ -185,7 +172,6 @@ return {
             vim.list_extend(ensure_installed, {
                 'lua_ls', -- Used to format Lua code
             })
-            -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
             require('mason-lspconfig').setup {
                 handlers = {
@@ -195,7 +181,7 @@ return {
                         -- by the server configuration above. Useful when disabling
                         -- certain features of an LSP (for example, turning off formatting for tsserver)
                         server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                        require('lspconfig')[server_name].setup(server)
+                        vim.lsp.config(server_name, server)
                     end,
                 },
                 ensure_installed = ensure_installed,
--- nvim/.config/nvim/lua/custom/themes.lua
+++ nvim/.config/nvim/lua/custom/themes.lua
@@ -11,6 +11,11 @@ return {
     {
         'folke/tokyonight.nvim',
         lazy = false,
-        priority = 1000, -- make sure to load this before all the other start plugins.
+        priority = 1000,
+    },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        priority = 1000,
     },
 }

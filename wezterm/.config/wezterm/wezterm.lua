3f2d3a6760aeaa8d50518ae5d5ae8794f946f028
Leonardo Vencovsky <lvenk26@gmail.com>
2025-09-09 18:22:41 +0200

    Update WezTerm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,19 +1,37 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- Color scheme
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = 'Catppuccin Mocha'
 
 -- Window Configuration
 config.initial_cols = 135
 config.initial_rows = 30
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 
 -- Tab Bar Configuration
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.show_tab_index_in_tab_bar = false
 config.use_fancy_tab_bar = false
+
+config.keys = {
+    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+    {
+        key = 'LeftArrow',
+        mods = 'OPT',
+        action = act.SendKey {
+            key = 'b',
+            mods = 'ALT',
+        },
+    },
+    {
+        key = 'RightArrow',
+        mods = 'OPT',
+        action = act.SendKey { key = 'f', mods = 'ALT' },
+    },
+}
 
 return config
